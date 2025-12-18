d07dd2841431c9fe69256397a18a6c9191d52dd7
Daniel Salgueiro <pogeboii@proton.me>
2025-12-18 07:13:54 +0000

    Implemented data coherance between Ledger and its' Assets
    Upon addition or removal of a transaction the ledger now updates its'
    assets attribute

--- src/ledger/Ledger.java
+++ src/ledger/Ledger.java
@@ -1,8 +1,10 @@
 package ledger;
 
+import bank.Account;
 import bank.Assets;
 import bank.Currency;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.NavigableMap;
@@ -55,6 +57,10 @@ public class Ledger {
 		if (dateRecords.contains(transaction)) {
 			return false;
 		}
+
+		Account account = transaction.getAccount();
+		account.transaction(transaction.getAmmount());
+
 		dateRecords.add(transaction);
 		return true;
 
@@ -68,6 +74,10 @@ public class Ledger {
 	public boolean removeTransaction(Transaction transaction) {
 		ArrayList<Transaction> dateRecords = this.ledger.get(transaction.getDate());
 		if (dateRecords != null && dateRecords.remove(transaction)) {
+			Account account = transaction.getAccount();
+			BigDecimal revertTransaction = transaction.getAmmount().negate();
+			account.transaction(revertTransaction);
+
 			return true;
 		}
 		return false;
@@ -77,7 +87,7 @@ public class Ledger {
 	 * Returns all transactions from the received date
 	 * @param date - Date of requested transactions
 	 */
-	public ArrayList<Transaction> getDayRecords(LocalDate date) {
+	public ArrayList<Transaction> getRecordsByDay(LocalDate date) {
 		return this.ledger.get(date);
 	}
 
@@ -85,7 +95,7 @@ public class Ledger {
 	 * Returns all transactions from the requested number of previous months
 	 * @param months - Number of months' records to return
 	 */
-	public NavigableMap<LocalDate, ArrayList<Transaction>> getMonthRecords(long months) {
+	public NavigableMap<LocalDate, ArrayList<Transaction>> getRecordsByMonths(long months) {
 		LocalDate currentDate = LocalDate.now();
 		LocalDate startDate = currentDate.minusMonths(months);
 		return this.ledger.subMap(startDate, true, currentDate, true);
